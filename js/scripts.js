3978d12025385122711e78eeb53d9557c4cf93c0
MIKOLAJ63 <MIKOLAJ63>
2017-08-24 22:20:22 +0200

    Random quote machine

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+//scripts.js
+$(function(){
+	console.log('DOM loaded - you can have fun');
+});
+
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var prefix = "https://cors-anywhere.herokuapp.com/";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
+function getQuote() {
+$.getJSON(prefix + quoteUrl, createTweet);
+$.ajaxSetup({ 
+cache: false
+});
+}
+
+function createTweet(input) {
+var data = input[0];
+var quoteText = $(data.content).text().trim();
+var quoteAuthor = data.title;
+  
+if (!quoteAuthor.length) {
+quoteAuthor = "unknown author";
+}
+var tweetText = "Quote of the day - " + quoteText + " author: " + quoteAuthor;
+if (tweetText.length > 140) {
+getQuote();
+} else {
+var tweet = tweetLink + encodeURIComponent(tweetText); 
+$('.quote').text(quoteText);
+$('.author').text("author: " + quoteAuthor);
+
+$('.tweet').attr('href', tweet);
+} 
+}
+
+$(document).ready(function() {
+getQuote();
+
+$('.trigger').click(function() {
+getQuote();
+})
+});
